8edb1627bf58dd1f87eb2d436e7d5e9a07f0ee5e
AKILENG ISAAC <isaacakileng56@gmail.com>
2017-10-02 16:42:27 +0300

    Initial jQueryLib

---
+++ src/jQueryLib/widget/jQueryLib.js
@@ -0,0 +1,10 @@
+//Empty Widget to inclulde jQuery Libs
+mxui.widget.declare("jQueryLib.widget.jQueryLib", {
+    inputargs: {
+        name:""
+    },
+    postCreate : function(){
+        this.actRendered();
+    }
+});
+
